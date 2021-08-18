cd6be5ec3386c8a235bf9885afbf3c886f094acc
BartoszOrnaf <bartosz.ornaf@gmail.com>
2021-08-18 12:48:21 +0200

    Patch API added

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 require('dotenv/config');
 
-//Middlewares
-// app.use('/posts', ()=>{
-//     console.log('This is a middleware running')
-// })
-
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Import Routes
-const postsRoute = require('./routes/posts')
+const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
 //Routes
-app.get('/', (req, res)=>{
-    res.send('We are on home')
-})
-
+app.get('/', (req, res) => {
+  res.send('We are on home');
+});
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true }, ()=>{
-    console.log('Connected to DB!')
-})
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB!');
+  }
+);
 
 //How to listen to the server
 app.listen(3000);
--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,69 @@
-const express  = require('express');
+const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+//Gets back all the post
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-router.get('/', (req, res)=>{
-    res.send('We are on posts')
-})
+//Submits a post
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-router.get('/specific', (req, res)=>{
-    res.send('We are on specific post')
-})
+//Specific post
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
 
-router.post('/', (req, res) =>{
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    })
 
-    post.save()
-    .then(data => {
-        res.json(data)
-    })
-    .catch(err => {
-        res.json({message: err})
-    })
-})
+});
+
+
+//Delete post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const removePost = await Post.remove({ _id: req.params.postId });
+    res.json(removePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Update a post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        $set: {
+          title: req.body.title,
+        },
+      }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 module.exports = router;
